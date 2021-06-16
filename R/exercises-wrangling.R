201dfd2614353c75c121debe51e7f9402659446f
Rikke Kruse <rkruse@health.sdu.dk>
2021-06-16 13:45:13 +0200

    Basic statistics

--- R/exercises-wrangling.R
+++ R/exercises-wrangling.R
@@ -15,3 +15,24 @@ nhanes_small %>%
 nhanes_small %>%
     select(phys_active_days, phys_active) %>%
     rename(days_phys_active = phys_active_days)
+
+
+## 9.16 Calculate som basic statistics
+
+# 1.
+nhanes_small %>%
+    summarise(mean_weight = mean(weight, na.rm = TRUE),
+              mean_age = mean(age, na.rm = TRUE))
+
+# 2.
+nhanes_small %>%
+    summarise(max_height = max(height, na.rm = TRUE),
+              min_height = min(height, na.rm = TRUE))
+
+# 3.
+nhanes_small %>%
+    summarise(median_age = (median(age, na.rm = TRUE))
+
+
+
+
