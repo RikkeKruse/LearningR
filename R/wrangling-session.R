019033a730e3ab4fc477973a59379b7e57c6eaae
Rikke Kruse <rkruse@health.sdu.dk>
2021-06-16 15:31:31 +0200

    save data

--- R/wrangling-session.R
+++ R/wrangling-session.R
@@ -178,7 +178,7 @@ nhanes_small %>%
     summarise(mean_age = mean(age, na.rm = TRUE), mean_bmi = mean(bmi, na.rm = TRUE)) %>%
     ungroup()
 
-# ALtid ungroup datasæt så det kan brues igen
+# ALtid ungroup datasæt så det kan bruges igen
 
 
 #Saving datasets as files
